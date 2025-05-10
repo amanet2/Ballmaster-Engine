102c4bf3abc66e7b788bf6db943035da5c44ce66
Stallion <tonyjmstallion@gmail.com>
2025-05-10 13:43:16 -0500

    better build script

--- debug/build_engine.sh
+++ debug/build_engine.sh
@@ -3,22 +3,21 @@ game_home="/Users/stallion/Code/Ballmaster-Game"
 java_bin="$engine_home/runtime/jdk-23.0.2.jdk/Contents/Home/bin"
 java_out="$engine_home/tmp"
 java_src="$engine_home/src/com/app/engine/*.java"
-jar_name="$engine_home/engine.jar"
+jar_name="engine.jar"
+jar_out="$java_out/$jar_name"
 
 prep() {
-  rm -f $jar_name
   rm -rf $java_out
 }
 
 build() {
   $java_bin/javac -d $java_out $java_src
-  $java_bin/jar cf $jar_name -C $java_out .
+  $java_bin/jar cf $jar_out -C $java_out .
 }
 
 cleanup() {
+  cp $jar_out $game_home/$jar_name
   rm -rf $java_out
-  rm -f $game_home/$jar_name
-  cp $jar_name $game_home
 }
 
 prep
