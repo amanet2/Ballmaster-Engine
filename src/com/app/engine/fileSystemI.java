08778dc375ae20dc156bc1e65929abab836b2517
Stallion <tonyjmstallion@gmail.com>
2025-05-20 21:26:50 -0500

    notes

--- src/com/app/engine/fileSystemI.java
+++ src/com/app/engine/fileSystemI.java
@@ -9,4 +9,17 @@ public interface fileSystemI {
         gFile[] getFiles();
         String[] getFileNames();
     }
+
+    // TODO: need ability to refresh/read directories multiple times
+
+    /**
+     * e.g. directory /foo contains files [ bar.c, baz.c ]
+     * gDirectory.readDirectory("/foo");  // returns String[]{"bar.c", "baz.c"}
+     * // qaz.c gets written to /foo by mapmaker or savefile system
+     * gDirectory.readDirectory("/foo");  // returns String[]{"bar.c", "baz.c", "qaz.c"}
+     */
+    interface gFileSystem {
+        gDirectory directory(String path);
+        gFile file(String file);
+    }
 }
