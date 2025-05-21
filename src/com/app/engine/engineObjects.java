f2bb2013704ff14750c8fee35359b938c9224dd2
Stallion <tonyjmstallion@gmail.com>
2025-05-20 20:32:17 -0500

    more engine objs

--- src/com/app/engine/engineObjects.java
+++ src/com/app/engine/engineObjects.java
@@ -2,12 +2,14 @@ package com.app.engine;
 
 public class engineObjects {
     public consoleSystem consoleSystem;
+    public cVarSystem cVarSystem;
     public graphicsSystem graphicsSystem;
     public schedulerSystem schedulerSystem;
     public spriteSystem spriteSystem;
 
     public engineObjects() {
         this.consoleSystem = new consoleSystem();
+        this.cVarSystem = new cVarSystem();
         this.graphicsSystem = new graphicsSystem();
         this.schedulerSystem = new schedulerSystem();
         this.spriteSystem = new spriteSystem();
