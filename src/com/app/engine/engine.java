03d00d02520c5afadfdc0bb02319e30479d214b2
Stallion <tonyjmstallion@gmail.com>
2025-05-21 22:02:15 -0500

    made the engine instance a singleton

--- src/com/app/engine/engine.java
+++ src/com/app/engine/engine.java
@@ -1,13 +1,21 @@
 package com.app.engine;
 
 public class engine {
+    private static engine instance;
+
+    public static engine instance() {
+        if(instance == null)
+            instance = new engine();
+        return instance;
+    }
+
     public consoleSystem consoleSystem;
     public cVarSystem cVarSystem;
     public graphicsSystem graphicsSystem;
     public schedulerSystem schedulerSystem;
     public spriteSystem spriteSystem;
 
-    public engine() {
+    private engine() {
         this.consoleSystem = new consoleSystem();
         this.cVarSystem = new cVarSystem();
         this.graphicsSystem = new graphicsSystem();
