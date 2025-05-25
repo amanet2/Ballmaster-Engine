aaa843a8b294aa9bd0df4037f1a1a074ffd67eac
Stallion <tonyjmstallion@gmail.com>
2025-05-24 20:02:25 -0500

    dict lexing

--- src/com/app/engine/utils.java
+++ src/com/app/engine/utils.java
@@ -2,6 +2,7 @@ package com.app.engine;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.TimeZone;
@@ -17,30 +18,70 @@ public class utils {
     }
 
     public static class gDict {
-        private HashMap<String, String> internalMap;
+        private HashMap<String, Object> internalMap;  // value can be String or gDict
+        public static char tokenOpenBracket = '{';
+        public static char tokenAssigner = '=';
+        public static char tokenSeparator = ',';
+        public static char tokenCloseBracket = '}';
+        public static char escapeCharacter = '^';  // TODO: handle escape chars in strings
 
         public gDict() {
             internalMap = new HashMap<>();
         }
 
-        public gDict(String stateString) {
-            // e.g. {foo=bar, baz=qaz}
+        public gDict(String dictString) {
+            // e.g. {foo=bar, baz={qaz=yaz}}
             internalMap = new HashMap<>();
-            stateString = stateString.trim();
-            String trimmedStateString = stateString.substring(1, stateString.length()-1);  // remove surrounding {}
-            for(String kvPair : trimmedStateString.split(",")) {
-                String[] kvPairTokens = kvPair.split("=");
-                String key = kvPairTokens[0].trim();
-                String value = kvPairTokens.length > 1 ? kvPairTokens[1].trim() : "";  // accomodate {foo=,bar=baz}
-                internalMap.put(key, value);
-            }
+
+            ArrayList<String> lexedDictStringTokens = lexDictString(dictString);
+            gDict parsedLexedDictStringTokens = parseLexedDictStringTokens(lexedDictStringTokens);
+
+            System.out.println("DICT LEXED TOKENS: " + lexedDictStringTokens);
         }
 
-        public void put(String key, String value) {
+        private ArrayList<String> lexDictString(String dictString) {
+            ArrayList<String> lexedDictStringTokens = new ArrayList<>();
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for(int i = 0; i < dictString.length(); i++) {
+                char charAtIndex = dictString.charAt(i);
+
+                if(charAtIndex == tokenOpenBracket
+                        || charAtIndex == tokenAssigner
+                        || charAtIndex == tokenSeparator
+                        || charAtIndex == tokenCloseBracket) {
+                    if(!stringBuilder.isEmpty()) {
+//                        lexedDictStringTokens.add("'" + stringBuilder.toString().trim() +"'");  // for debugging
+                        lexedDictStringTokens.add(stringBuilder.toString().trim());
+                        stringBuilder = new StringBuilder();
+                    }
+
+//                    lexedDictStringTokens.add("'" + Character.toString(charAtIndex) + "'");  // for debugging
+                    lexedDictStringTokens.add(Character.toString(charAtIndex));
+                }
+                else
+                    stringBuilder.append(dictString.charAt(i));
+            }
+
+            return lexedDictStringTokens;
+        }
+
+        private gDict parseLexedDictStringTokens(ArrayList<String> lexedDictStringTokens) {
+            gDict parsedDict = new gDict();
+
+//            for(int i = 0; i < lexedDictStringTokens.size(); i++) {
+//                if(lexedDictStringTokens.equals(Character.toString(tokenOpenBracket)))
+//            }
+
+            return parsedDict;
+        }
+
+        public void put(String key, Object value) {
             internalMap.put(key, value);
         }
 
-        public String get(String key) {
+        public Object get(String key) {
             return internalMap.get(key);
         }
 
@@ -53,11 +94,23 @@ public class utils {
         }
 
         public String toString() {
+            // TODO: implement escape chars
+//            for(char reservedChar : new char[]{gDict.tokenOpenBracket, gDict.tokenAssigner, gDict.tokenSeparator, gDict.escapeCharacter}) {
+//                kValue = kValue.replace(Character.toString(reservedChar), "^"+reservedChar);
+//            }
             return internalMap.toString();
         }
     }
 
     public static class gMath {
+        public static double scaleDoubleToWindowHeight(double input, double scale, int windowHeight) {
+            return (input / scale) * (double) windowHeight;
+        }
+
+        public static double unscaleDoubleToWindowHeight(double input, double scale, int windowHeight) {
+            return (input * scale) / (double) windowHeight;
+        }
+
         public static int scaleIntToWindowHeight(int inputInt, int scale, int windowHeight) {
             return (int) ((((double) inputInt / (double) scale) * (double) windowHeight));
         }
